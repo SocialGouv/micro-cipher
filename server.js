c108261801e5239b79c168af8f9d208ae6eb64f3
Matéo Mévollon <m.mevollon@protonmail.com>
2022-02-28 17:28:04 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+import { createServer } from "http"
+import { generateKeyPair, privateDecrypt } from "crypto"
+import { promisify } from "util"
+
+// Generate a one-run RSA key pair
+const { publicKey, privateKey } = await promisify(generateKeyPair)('rsa', {
+  modulusLength: 4096,
+  publicKeyEncoding: {
+    type: 'spki',
+    format: 'pem'
+  },
+  privateKeyEncoding: {
+    type: 'pkcs8',
+    format: 'pem'
+  }
+})
+
+// Print the public key to the admin who just started this app
+console.log(publicKey)
+console.log("Encrypt your secrets using this key and the folowing openssl command:")
+console.log("openssl rsautl -encrypt -oaep -pubin -inkey <rsa_public_key_file>");
+
+// Start a server to decrypt on demand
+const requestListener = async (req, res) => {
+  const dataBuffers = []
+
+  // We might receive data in multiple chunks
+  for await (const chunk of req) {
+    dataBuffers.push(chunk)
+  }
+  const dataBuffer = Buffer.concat(dataBuffers)
+
+  // Send back decrypted data using the in-memory key
+  res.writeHead(200)
+  res.end(privateDecrypt(privateKey, dataBuffer).toString())
+}
+
+const server = createServer(requestListener)
+server.listen(3000)
